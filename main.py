9acb1ccdcd880c1605f1d86ae368ef58ded1f8c5
Stefan Tasnadi <stefytasnadi@yahoo.com>
2021-11-28 19:03:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from pypresence import Presence
 import time
 
-client_id = 'YOU CLIENT ID HERE' #Put your client ID here
+client_id = '914571824749228052'
 RPC = Presence(client_id) 
 RPC.connect() 
 
-print(RPC.update(state="STATE HERE", details="DETAILS HERE", large_image="NAME OF LARGE IMAGE HERE", small_image="NAME OF SMALL IMAGE HERE", large_text="LARGE IMAGE TEXT HERE", start=time.time()))  # Set the presence
+print(RPC.update(state="Project Diablo II", details="Farming Runes", large_image="NAME OF LARGE IMAGE HERE", small_image="NAME OF SMALL IMAGE HERE", large_text="LARGE IMAGE TEXT HERE", start=time.time()))  # Set the presence
 
 while True: 
     time.sleep(15) 
